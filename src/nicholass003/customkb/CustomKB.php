45fae4b9261b87b2649b9009a3f4f5cf09c236e1
nicholass003 <veganicholas123@gmail.com>
2024-04-14 19:15:09 +0700

    fix: cs

--- src/nicholass003/customkb/CustomKB.php
+++ src/nicholass003/customkb/CustomKB.php
@@ -30,8 +30,8 @@ use dktapps\pmforms\element\Input;
 use dktapps\pmforms\FormIcon;
 use dktapps\pmforms\MenuForm;
 use dktapps\pmforms\MenuOption;
-use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\entity\Living;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\Listener;
@@ -39,197 +39,204 @@ use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
+use function array_merge;
+use function count;
+use function filter_var;
+use function in_array;
+use function strtolower;
+use const FILTER_VALIDATE_FLOAT;
+use const FILTER_VALIDATE_INT;
 
 class CustomKB extends PluginBase implements Listener{
 
-    public const PREFIX = "§6[§eCustomKB§6]§r\n";
+	public const PREFIX = "§6[§eCustomKB§6]§r\n";
 
-    /** @var string[] */
-    private array $disabledWorlds = [];
+	/** @var string[] */
+	private array $disabledWorlds = [];
 
-    public const TYPE_ATTACKDELAY = 0;
-    public const TYPE_KNOCKBACK = 1;
+	public const TYPE_ATTACKDELAY = 0;
+	public const TYPE_KNOCKBACK = 1;
 
-    public Config $data;
+	public Config $data;
 
-    protected function onLoad() : void{
-        $this->saveDefaultConfig();
+	protected function onLoad() : void{
+		$this->saveDefaultConfig();
 
-        $disabledWorlds = $this->getConfig()->get("disabled-worlds", []);
-        if(count($disabledWorlds) > 0){
-            foreach($disabledWorlds as $disabledWorld){
-                $this->disabledWorlds[] = $disabledWorld;
-            }
-        }
-    }
+		$disabledWorlds = $this->getConfig()->get("disabled-worlds", []);
+		if(count($disabledWorlds) > 0){
+			foreach($disabledWorlds as $disabledWorld){
+				$this->disabledWorlds[] = $disabledWorld;
+			}
+		}
+	}
 
-    protected function onEnable() : void{
-        $this->data = new Config($this->getDataFolder() . "data.json", Config::JSON);
+	protected function onEnable() : void{
+		$this->data = new Config($this->getDataFolder() . "data.json", Config::JSON);
 
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    }
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
 
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-        if($sender instanceof Player){
-            if($command->getName() === "customkb"){
-                $worldName = $sender->getWorld()->getFolderName();
-                if(isset($args[0])){
-                    switch(strtolower($args[0])){
-                        case "help":
-                            $sender->sendMessage(self::PREFIX . TextFormat::YELLOW . "Help Information");
-                            $sender->sendMessage(TextFormat::GREEN . "- /help Help Information");
-                            $sender->sendMessage(TextFormat::GREEN . "- /options Options Information");
-                            $sender->sendMessage(TextFormat::GREEN . "- /set <options> <value> Setup CustomKB");
-                            $sender->sendMessage(TextFormat::GREEN . "- /settings Open Settings UI (aliases /ui)");
-                            break;
-                        case "option":
-                        case "options":
-                            $sender->sendMessage(self::PREFIX . TextFormat::YELLOW . "Options Information");
-                            $sender->sendMessage(TextFormat::GREEN . "- attackdelay (Set Custom Attackdelay to the World)");
-                            $sender->sendMessage(TextFormat::GREEN . "- knockback (Set Custom Knockback to the World)");
-                            break;
-                        case "set":
-                            if(isset($args[1])){
-                                if(isset($args[2])){
-                                    $type = match(strtolower($args[2])){
-                                        "attackdelay" => self::TYPE_ATTACKDELAY,
-                                        "knockback" => self::TYPE_KNOCKBACK,
-                                        default => throw new \InvalidArgumentException("Invalid type value")
-                                    };
-                                    $configure = $this->configure($worldName, $type, (float) $args[2]);
-                                    $this->sendMessage($sender, $this->getTarget($type), (float) $args[2], $configure);
-                                }else{
-                                    $sender->sendMessage(TextFormat::RED . "Usage: /customkb set " . $args[1] . " <value>");
-                                }
-                            }else{
-                                $sender->sendMessage(TextFormat::RED . "Usage: /customkb set [options]");
-                            }
-                            break;
-                        case "setting":
-                        case "settings":
-                        case "ui":
-                            $this->openSettingsForm($sender);
-                            break;
-                        case "show":
-                            $attackDelay = $this->get($worldName, self::TYPE_ATTACKDELAY) ?? "default";
-                            $knockBack = $this->get($worldName, self::TYPE_KNOCKBACK) ?? "default";
-                            $sender->sendMessage(self::PREFIX . TextFormat::YELLOW . "CustomKB Information\n" . TextFormat::YELLOW . "World: " . $worldName . "\n");
-                            $sender->sendMessage(TextFormat::GREEN . "AttackDelay : {$attackDelay}\n" . TextFormat::GREEN . "KnockBack : {$knockBack}");
-                            break;
-                    }
-                }else{
-                    $sender->sendMessage(TextFormat::RED . "Usage: /customkb <help:options:set:settings");
-                }
-            }
-        }
-        return true;
-    }
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		if($sender instanceof Player){
+			if($command->getName() === "customkb"){
+				$worldName = $sender->getWorld()->getFolderName();
+				if(isset($args[0])){
+					switch(strtolower($args[0])){
+						case "help":
+							$sender->sendMessage(self::PREFIX . TextFormat::YELLOW . "Help Information");
+							$sender->sendMessage(TextFormat::GREEN . "- /help Help Information");
+							$sender->sendMessage(TextFormat::GREEN . "- /options Options Information");
+							$sender->sendMessage(TextFormat::GREEN . "- /set <options> <value> Setup CustomKB");
+							$sender->sendMessage(TextFormat::GREEN . "- /settings Open Settings UI (aliases /ui)");
+							break;
+						case "option":
+						case "options":
+							$sender->sendMessage(self::PREFIX . TextFormat::YELLOW . "Options Information");
+							$sender->sendMessage(TextFormat::GREEN . "- attackdelay (Set Custom Attackdelay to the World)");
+							$sender->sendMessage(TextFormat::GREEN . "- knockback (Set Custom Knockback to the World)");
+							break;
+						case "set":
+							if(isset($args[1])){
+								if(isset($args[2])){
+									$type = match(strtolower($args[2])){
+										"attackdelay" => self::TYPE_ATTACKDELAY,
+										"knockback" => self::TYPE_KNOCKBACK,
+										default => throw new \InvalidArgumentException("Invalid typevalue")
+									};
+									$configure = $this->configure($worldName, $type, (float) $args[2]);
+									$this->sendMessage($sender, $this->getTarget($type), (float) $args[2], $configure);
+								}else{
+									$sender->sendMessage(TextFormat::RED . "Usage: /customkb set " . $args[1] . " <value>");
+								}
+							}else{
+								$sender->sendMessage(TextFormat::RED . "Usage: /customkb set [options]");
+							}
+							break;
+						case "setting":
+						case "settings":
+						case "ui":
+							$this->openSettingsForm($sender);
+							break;
+						case "show":
+							$attackDelay = $this->get($worldName, self::TYPE_ATTACKDELAY) ?? "default";
+							$knockBack = $this->get($worldName, self::TYPE_KNOCKBACK) ?? "default";
+							$sender->sendMessage(self::PREFIX . TextFormat::YELLOW . "CustomKB Information\n" . TextFormat::YELLOW . "World: " . $worldName . "\n");
+							$sender->sendMessage(TextFormat::GREEN . "AttackDelay : {$attackDelay}\n" . TextFormat::GREEN . "KnockBack : {$knockBack}");
+							break;
+					}
+				}else{
+					$sender->sendMessage(TextFormat::RED . "Usage: /customkb <help:options:set:settings");
+				}
+			}
+		}
+		return true;
+	}
 
-    public function onEntityDamageByEntity(EntityDamageByEntityEvent $event) : void{
-        $victim = $event->getEntity();
-        $damager = $event->getDamager();
-        if($victim instanceof Living && $damager instanceof Player){
-            $worldName = $victim->getWorld()->getFolderName();
-            if(!in_array($worldName, $this->disabledWorlds)){
-                $attackCd = $this->get($worldName, self::TYPE_ATTACKDELAY);
-                if($attackCd !== null){
-                    $event->setAttackCooldown((int) $attackCd);
-                }
-                $knockBack = $this->get($worldName, self::TYPE_KNOCKBACK);
-                if($knockBack !== null){
-                    $event->setKnockBack((float) $knockBack);
-                }
-            }
-        }
-    }
+	public function onEntityDamageByEntity(EntityDamageByEntityEvent $event) : void{
+		$victim = $event->getEntity();
+		$damager = $event->getDamager();
+		if($victim instanceof Living && $damager instanceof Player){
+			$worldName = $victim->getWorld()->getFolderName();
+			if(!in_array($worldName, $this->disabledWorlds, true)){
+				$attackCd = $this->get($worldName, self::TYPE_ATTACKDELAY);
+				if($attackCd !== null){
+					$event->setAttackCooldown((int) $attackCd);
+				}
+				$knockBack = $this->get($worldName, self::TYPE_KNOCKBACK);
+				if($knockBack !== null){
+					$event->setKnockBack((float) $knockBack);
+				}
+			}
+		}
+	}
 
-    public function openSettingsForm(Player $player) : void{
-        $form = new MenuForm(
-            "§eCustomKB Settings",
-            "§fConfigure your CustomKB",
-            [
-                new MenuOption("AttackDelay", new FormIcon("textures/items/clock_item", FormIcon::IMAGE_TYPE_PATH)),
-                new MenuOption("KnockBack", new FormIcon("textures/items/fireball", FormIcon::IMAGE_TYPE_PATH))
-            ],
-            function(Player $submitter, int $option) : void{
-                switch($option){
-                    case 0:
-                        $this->openSetupForm($submitter, self::TYPE_ATTACKDELAY);
-                        break;
-                    case 1:
-                        $this->openSetupForm($submitter, self::TYPE_KNOCKBACK);
-                        break;
-                }
-            }
-        );
-        $player->sendForm($form);
-    }
+	public function openSettingsForm(Player $player) : void{
+		$form = new MenuForm(
+			"§eCustomKB Settings",
+			"§fConfigure your CustomKB",
+			[
+				new MenuOption("AttackDelay", new FormIcon("textures/items/clock_item", FormIcon::IMAGE_TYPE_PATH)),
+				new MenuOption("KnockBack", new FormIcon("textures/items/fireball", FormIcon::IMAGE_TYPE_PATH))
+			],
+			function(Player $submitter, int $option) : void{
+				switch($option){
+					case 0:
+						$this->openSetupForm($submitter, self::TYPE_ATTACKDELAY);
+						break;
+					case 1:
+						$this->openSetupForm($submitter, self::TYPE_KNOCKBACK);
+						break;
+				}
+			}
+		);
+		$player->sendForm($form);
+	}
 
-    public function openSetupForm(Player $player, int $type) : void{
-        $target = $this->getTarget($type);
-        $form = new CustomForm(
-            "§e{$target} Settings",
-            [
-                new Input("input_kb", "§fInput {$target} value", "§fvalue must be more than 0")
-            ],
-            function(Player $submitter, CustomFormResponse $response) use($target, $type) : void{
-                $value = $this->getValue($response->getString("input_kb"));
-                if($value === false){
-                    $submitter->sendMessage(TextFormat::RED . "Failed to submit, numeric value only!");
-                    return;
-                }
-                $this->sendMessage($submitter, $target, $value, $this->configure($submitter->getWorld()->getFolderName(), $type, $value));
-            }
-        );
-        $player->sendForm($form);
-    }
+	public function openSetupForm(Player $player, int $type) : void{
+		$target = $this->getTarget($type);
+		$form = new CustomForm(
+			"§e{$target} Settings",
+			[
+				new Input("input_kb", "§fInput {$target} value", "§fvalue must be more than 0")
+			],
+			function(Player $submitter, CustomFormResponse $response) use($target, $type) : void{
+				$value = $this->getValue($response->getString("input_kb"));
+				if($value === false){
+					$submitter->sendMessage(TextFormat::RED . "Failed to submit, numeric value only!");
+					return;
+				}
+				$this->sendMessage($submitter, $target, $value, $this->configure($submitter->getWorld()->getFolderName(), $type, $value));
+			}
+		);
+		$player->sendForm($form);
+	}
 
-    private function configure(string $worldName, int $type, float $value) : bool{
-        if(in_array($worldName, $this->disabledWorlds) || $value <= 0){
-            return false;
-        }
-        if($type === self::TYPE_ATTACKDELAY){
-            $value = (int) $value;
-        }
-        $target = strtolower($this->getTarget($type));
-        $data = $this->data->get($worldName);
-        if(!empty($data)){
-            $this->data->set($worldName, array_merge($data, [$target => $value]));
-        }else{
-            $this->data->set($worldName, [$target => $value]);
-        }
-        $this->data->save();
-        return true;
-    }
+	private function configure(string $worldName, int $type, float $value) : bool{
+		if(in_array($worldName, $this->disabledWorlds, true) || $value <= 0){
+			return false;
+		}
+		if($type === self::TYPE_ATTACKDELAY){
+			$value = (int) $value;
+		}
+		$target = strtolower($this->getTarget($type));
+		$data = $this->data->get($worldName);
+		if(!empty($data)){
+			$this->data->set($worldName, array_merge($data, [$target => $value]));
+		}else{
+			$this->data->set($worldName, [$target => $value]);
+		}
+		$this->data->save();
+		return true;
+	}
 
-    private function get(string $worldName, int $type) : float|int|null{
-        $target = strtolower($this->getTarget($type));
-        $data = $this->data->get($worldName);
-        return $data[$target] ?? null;
-    }
+	private function get(string $worldName, int $type) : float|int|null{
+		$target = strtolower($this->getTarget($type));
+		$data = $this->data->get($worldName);
+		return $data[$target] ?? null;
+	}
 
-    private function sendMessage(Player $player, string $target, float|int $value, bool $configure) : void{
-        if($configure === true){
-            $player->sendMessage("§aSuccess configure {$target} with value {$value} !");
-        }else{
-            $player->sendMessage("§cFailed configure {$target} with value {$value} !");
-        }
-    }
+	private function sendMessage(Player $player, string $target, float|int $value, bool $configure) : void{
+		if($configure === true){
+			$player->sendMessage("§aSuccess configure {$target} with value {$value} !");
+		}else{
+			$player->sendMessage("§cFailed configure {$target} with value {$value} !");
+		}
+	}
 
-    private function getTarget(int $type) : string{
-        return match($type){
-            self::TYPE_ATTACKDELAY => "AttackDelay",
-            self::TYPE_KNOCKBACK => "KnockBack",
-            default => throw new \InvalidArgumentException("Unknown type target type: " . $type)
-        };
-    }
+	private function getTarget(int $type) : string{
+		return match($type){
+			self::TYPE_ATTACKDELAY => "AttackDelay",
+			self::TYPE_KNOCKBACK => "KnockBack",
+			default => throw new \InvalidArgumentException("Unknown type target type: " . $type)
+		};
+	}
 
-    private function getValue(string $stringValue) : false|float|int{
-        if(filter_var($stringValue, FILTER_VALIDATE_FLOAT) !== false){
-            return (float) $stringValue;
-        }elseif(filter_var($stringValue, FILTER_VALIDATE_INT) !== false){
-            return (int) $stringValue;
-        }
-        return false;
-    }
+	private function getValue(string $stringValue) : false|float|int{
+		if(filter_var($stringValue, FILTER_VALIDATE_FLOAT) !== false){
+			return (float) $stringValue;
+		}elseif(filter_var($stringValue, FILTER_VALIDATE_INT) !== false){
+			return (int) $stringValue;
+		}
+		return false;
+	}
 }
